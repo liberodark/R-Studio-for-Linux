26167e8499ae7b4500465bf4ad51ab4072d798ef
liberodark <liberodark@gmail.com>
2017-09-06 20:39:50 +0200

    Install

---
+++ Install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# About: Install R-Studio automatically
+# Author: liberodark
+# License: GNU GPLv3
+
+# Download Repo
+
+git clone https://github.com/liberodark/R-Studio
+
+cd R-Studio
+
+# Download Software
+
+wget http://www.r-studio.com/downloads/RStudio4_x64.deb
+
+# Check MD5
+
+md5sum RStudio4_x64.deb
+
+# Run Build pkg.tar.xz
+
+makepkg
+
+# Install
+
+#pacman -U *
+
+# Clean
+
+#cd ..
+
+#rm -r R-Studio
